c5359c2bd0b7391679eee36c84b1d73cbb5dcbe3
AnkitSharma785 <119285460+AnkitSharma785@users.noreply.github.com>
2025-04-03 14:33:21 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+      val textView : TextView = findViewById(R.id.textView)
+      val helloText = StringBuilder()
+
+      repeat(times = 10) {index ->
+          helloText.append("Hello World! #${index +1}\n")
+      }
+
+        textView.text = helloText.toString()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
